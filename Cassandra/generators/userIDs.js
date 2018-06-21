56509971b7522f279c8a77f9fc34d8783b223f5a
Catherine Ligad <kateligad@gmail.com>
2018-06-21 15:56:09 -0700

    created a function for generating and keeping 50 userIDs

--- Cassandra/generators/userIDs.js
+++ Cassandra/generators/userIDs.js
@@ -1,7 +1,6 @@
 const uuidv4 = require('uuid/v4');
 const fs = require('fs');
 
-
 const createUsersCSV = (number = 50) => {
   const userIDs = [];
   for (let i = 0; i <= number; i += 1) {
@@ -16,12 +15,4 @@ const createUsersCSV = (number = 50) => {
   wstream.write(userIDs.join(','));
 };
 createUsersCSV();
-
-
-async function getAllUserIDs() {
-  const userIDs = `${__dirname}/CSVs/userIDs.txt`;
-  const text = await fs.readFile(userIDs, 'utf8');
-  return (text.split(','));
-}
-module.exports = getAllUserIDs;
 
