a3f12240645eabeb4e23f76e9c70db50d17ebea1
Catherine Ligad <kateligad@gmail.com>
2018-06-23 16:37:53 -0700

    starting to work on seeding db

--- Postgress/database/post.js
+++ Postgress/database/post.js
@@ -6,5 +6,40 @@ from comments
 inner join users on comments.userid = users.userid
 where comments.hostelid = NUMBER;
 
+create table comments (
+  commentid uuid,
+  userid uuid, 
+  hostelid int,
+  created_at timestamp,
+  rate smallint,
+  ratedFeatures int ARRAY[7],
+  language varchar(50),
+  text text,
+  propertyresponse text
+);
+
+create table users (
+  userid uuid,
+  userCreatedAt timestamp,
+  firstname varchar(50),
+  lastname varchar(50),
+  username varchar(50),
+  age smallint,
+  email varchar(50),
+  status varchar (20),
+  country varchar(50),
+  numofreviews smallint
+);
+
+create table ratedfeatures (
+  hostelid smallint,
+  valueForMoney smallint,
+  security smallint,
+  location smallint,
+  staff smallint,
+  atmosphere smallint,
+  cleanliness smallint,
+  facilities smallint
+);
 
 */
