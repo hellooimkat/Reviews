3388c0ac930078af71a3e596ef83f521398854ff
Catherine Ligad <kateligad@gmail.com>
2018-06-29 14:57:33 -0700

    deleted files that were not needed or used

--- Cassandra/database/cass.js
+++
@@ -1,7 +0,0 @@
-const cassandra = require('cassandra-driver');
-const util = require('util');
-
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
-
-client.execute = util.promisify(client.execute);
-
