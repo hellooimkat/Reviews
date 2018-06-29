6eec7f267fe132e7e0a5c7b9cc33b0433ff0475a
Catherine Ligad <kateligad@gmail.com>
2018-06-29 14:01:37 -0700

    refactored one endpoint

--- Postgress/database/post.js
+++ Postgress/database/post.js
@@ -29,7 +29,7 @@ app.get('/api/reviews/overview/:id', (req, res) => {
 
   const qHostelOverview = `SELECT 
   totalReviewCount, avgRating, ratedFeatures 
-  FROM hostels WHERE hostelid = ${id}}`;
+  FROM hostels WHERE hostelid = ${id}`;
 
   const qCommentsOverview = `SELECT
   comments.created_at, comments.rate, comments.text,
@@ -46,37 +46,37 @@ app.get('/api/reviews/overview/:id', (req, res) => {
   let toReturn = {};
   Promise.all([hostelOverview, commentsOverview])
     .then( result => {
-      toReturn['avgRating'] = result[0][0]['avgrating'];
-      toReturn['totalReviewCount'] = result[0][0]['totalreviewcount'];
+      toReturn['avgRating'] = result[0].rows[0]['avgrating'];
+      toReturn['totalReviewCount'] = result[0].rows[0]['totalreviewcount'];
       toReturn['countryCount'] = {US: 1};
       toReturn['ratedFeatures'] = [
         {feature: 'Value For Money', 
-        rating: result[0][0]['ratedfeatures'][0]},
+        rating: result[0].rows[0]['ratedfeatures'][0]},
         {feature: 'Security', 
-        rating: result[0][0]['ratedfeatures'][1]},
+        rating: result[0].rows[0]['ratedfeatures'][1]},
         {feature: 'Location', 
-        rating: result[0][0]['ratedfeatures'][2]},
+        rating: result[0].rows[0]['ratedfeatures'][2]},
         {feature: 'Staff', 
-        rating: result[0][0]['ratedfeatures'][3]},
+        rating: result[0].rows[0]['ratedfeatures'][3]},
         {feature: 'Atmosphere', 
-        rating: result[0][0]['ratedfeatures'][4]},
+        rating: result[0].rows[0]['ratedfeatures'][4]},
         {feature: 'Cleanliness', 
-        rating: result[0][0]['ratedfeatures'][5]},
+        rating: result[0].rows[0]['ratedfeatures'][5]},
         {feature: 'Facilities', 
-        rating: result[0][0]['ratedfeatures'][6]},
+        rating: result[0].rows[0]['ratedfeatures'][6]},
       ];
       toReturn['reviews'] = [];
 
       for (let i = 0; i < 4; i++) {
         toReturn['reviews'].push({
-            created_at: result[1][i]['created_at'],
-            rate: result[1][i]['rate'],
-            text: result[1][i]['text'],
+            created_at: result[1].rows[i]['created_at'],
+            rate: result[1].rows[i]['rate'],
+            text: result[1].rows[i]['text'],
             user: {
-              username: result[1][i]['username'],
-              age: result[1][i]['age'],
-              status: result[1][i]['status'],
-              country: result[1][i]['country'],
+              username: result[1].rows[i]['username'],
+              age: result[1].rows[i]['age'],
+              status: result[1].rows[i]['status'],
+              country: result[1].rows[i]['country'],
         }});
       }
       res.status(200).send(toReturn);
@@ -84,76 +84,76 @@ app.get('/api/reviews/overview/:id', (req, res) => {
 });
 
 
-app.get('/api/reviews/:id/all', (req, res) => {
-  console.log('QUERY ==============', req.query);
+// app.get('/api/reviews/:id/all', (req, res) => {
+//   console.log('QUERY ==============', req.query);
 
-  const { id } = req.params;
-  const { pageNum } = req.query;
-  const { eng } = req.query;
-  const { sortBy } = req.query;
+//   const { id } = req.params;
+//   const { pageNum } = req.query;
+//   const { eng } = req.query;
+//   const { sortBy } = req.query;
 
-  let qTotal;
-  let languagePlug;
-  let qReviews = 'SELECT c.created_at, c.language, c.propertyresponse, c.text, c.rate, c.commentid, u.age, u.numofreviews, u.status, u.username, u.country FROM comments AS c INNER JOIN users AS u ON c.userid = u.userid WHERE ';
+//   let qTotal;
+//   let languagePlug;
+//   let qReviews = 'SELECT c.created_at, c.language, c.propertyresponse, c.text, c.rate, c.commentid, u.age, u.numofreviews, u.status, u.username, u.country FROM comments AS c INNER JOIN users AS u ON c.userid = u.userid WHERE ';
 
-  if (eng === 'true') {
-    qReviews = qReviews + ` c.hostelid = ${id} AND c.language = 'ENG'`;
-    qTotal = `SELECT totalengreviews FROM hostels WHERE hostelid = ${id};`;
-    languagePlug = 'totalengreviews';
-  } else if (eng === 'false') {
-    qReviews = qReviews + ` c.hostelid = ${id} AND c.language IN ('ENG', 'OTH')`;
-    qTotal = `SELECT totalreviewcount FROM hostels WHERE hostelid = ${id};`;
-    languagePlug = 'totalreviewcount'
-  }
+//   if (eng === 'true') {
+//     qReviews = qReviews + ` c.hostelid = ${id} AND c.language = 'ENG'`;
+//     qTotal = `SELECT totalengreviews FROM hostels WHERE hostelid = ${id};`;
+//     languagePlug = 'totalengreviews';
+//   } else if (eng === 'false') {
+//     qReviews = qReviews + ` c.hostelid = ${id} AND c.language IN ('ENG', 'OTH')`;
+//     qTotal = `SELECT totalreviewcount FROM hostels WHERE hostelid = ${id};`;
+//     languagePlug = 'totalreviewcount'
+//   }
 
-  if (sortBy === 'newest') {
-    qReviews = qReviews + ' ORDER BY c.created_at DESC';
-  } else if (sortBy === 'oldest') {
-    qReviews = qReviews + ' ORDER BY c.created_at ASC';
-  } else if (sortBy === 'topRated') {
-    qReviews = qReviews + ' ORDER BY c.rate DESC';
-  } else if (sortBy === 'lowestRated') {
-    qReviews = qReviews + ' ORDER BY c.rate ASC';
-  } else if (sortBy === 'ageGroup') {
-    qReviews = qReviews + ' ORDER BY u.age ASC';
-  }
+//   if (sortBy === 'newest') {
+//     qReviews = qReviews + ' ORDER BY c.created_at DESC';
+//   } else if (sortBy === 'oldest') {
+//     qReviews = qReviews + ' ORDER BY c.created_at ASC';
+//   } else if (sortBy === 'topRated') {
+//     qReviews = qReviews + ' ORDER BY c.rate DESC';
+//   } else if (sortBy === 'lowestRated') {
+//     qReviews = qReviews + ' ORDER BY c.rate ASC';
+//   } else if (sortBy === 'ageGroup') {
+//     qReviews = qReviews + ' ORDER BY u.age ASC';
+//   }
 
-  if (pageNum !== '1') {
-    qReviews = qReviews + ` OFFSET ${(Number(pageNum) * 10) - 10}`;
-  }
+//   if (pageNum !== '1') {
+//     qReviews = qReviews + ` OFFSET ${(Number(pageNum) * 10) - 10}`;
+//   }
 
-  qReviews = qReviews + ' LIMIT 10;';
+//   qReviews = qReviews + ' LIMIT 10;';
 
-  console.log(qTotal, '---------------------', qReviews)
+//   console.log(qTotal, '---------------------', qReviews)
   
-  const totalReviewsInCat = db.query(qTotal);
-  const reviewsSnippets = db.query(qReviews);
+//   const totalReviewsInCat = db.query(qTotal);
+//   const reviewsSnippets = db.query(qReviews);
 
-  let toReturn = {};
-  Promise.all([totalReviewsInCat, reviewsSnippets])
-    .then( result => {
-      toReturn['total'] = result[0][`${languagePlug}`];
-      toReturn['reviewSnippet'] = [];
+//   let toReturn = {};
+//   Promise.all([totalReviewsInCat, reviewsSnippets])
+//     .then( result => {
+//       toReturn['total'] = result[0][`${languagePlug}`];
+//       toReturn['reviewSnippet'] = [];
       
-      for(let i = 0; i < result[1].length; i++) {
-        console.log(result[1][i]['age'], result[1][i]['created_at'], result[1][i]['rate'], result[1][i]['language'])
+//       for(let i = 0; i < result[1].length; i++) {
+//         console.log(result[1][i]['age'], result[1][i]['created_at'], result[1][i]['rate'], result[1][i]['language'])
 
-        toReturn['reviewSnippet'].push({
-          country: result[1][i]['country'],
-          created_at: result[1][i]['created_at'],
-          language: result[1][i]['language'],
-          propertyResponse: result[1][i]['propertyresponse'],
-          text: result[1][i]['text'],
-          rate: result[1][i]['rate'],
-          age: result[1][i]['age'],
-          numOfReviews: result[1][i]['numofreviews'],
-          status: result[1][i]['status'],
-          username: result[1][i]['username'],
-      })};
-      res.status(200).send(toReturn);
-    })
-    .catch( err => console.log(err));
-});
+//         toReturn['reviewSnippet'].push({
+//           country: result[1][i]['country'],
+//           created_at: result[1][i]['created_at'],
+//           language: result[1][i]['language'],
+//           propertyResponse: result[1][i]['propertyresponse'],
+//           text: result[1][i]['text'],
+//           rate: result[1][i]['rate'],
+//           age: result[1][i]['age'],
+//           numOfReviews: result[1][i]['numofreviews'],
+//           status: result[1][i]['status'],
+//           username: result[1][i]['username'],
+//       })};
+//       res.status(200).send(toReturn);
+//     })
+//     .catch( err => console.log(err));
+// });
 
 // SELECT c.created_at, c.language, c.rate, u.age FROM comments AS c INNER JOIN users AS u ON c.userid = u.userid WHERE  c.hostelid = 60000 AND c.language = 'ENG' ORDER BY c.rate DESC LIMIT 10;
 
